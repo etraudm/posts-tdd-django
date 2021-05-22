743c3ed8a3ed0b01a99a02df0bdd36bed7331382
micael <duartemicael32@gmail.com>
2021-05-22 15:52:20 -0300

    test: first interface test

---
+++ posts/tests.py
@@ -0,0 +1,23 @@
+import os
+from pathlib import Path
+
+from django.test import TestCase, LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+class PostsTestCase(LiveServerTestCase):
+
+    def setUp(self):
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--disable-gpu')
+        self.browser = webdriver.Chrome(
+            executable_path=os.path.join(Path(__file__).resolve().parent.parent, 'utils/chromedriver.exe'),
+            options=options)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_should_open_chrome_window(self):
+        self.browser.get(self.live_server_url)
