f10e99ff3b324988977ef336fcd16d5c05c0dd4f
micael <duartemicael32@gmail.com>
2021-05-22 23:22:59 -0300

    feat: add model post_comments

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,2 +1,3 @@
 from .time_stampable_mixin import TimestampableMixin
 from .post import Post
+from .post_comments import *
---
+++ backend/models/post_comments.py
@@ -0,0 +1,16 @@
+import uuid
+
+from django.contrib.auth.models import User
+from django.db import models
+
+from backend.models import TimestampableMixin, Post
+
+
+class PostComments(TimestampableMixin, models.Model):
+    id = models.UUIDField(help_text="Post Comment ID", default=uuid.uuid4().hex, editable=False, primary_key=True)
+    user = models.ForeignKey(User, help_text="User", null=False, blank=False, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, help_text="Post", null=False, blank=False, on_delete=models.CASCADE)
+    body = models.TextField("Body", null=False, blank=False)
+
+    def __str__(self):
+        return '{}-{}'.format(self.post_id, self.body)
