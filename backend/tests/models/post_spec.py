26a80b6834504dc6a2c02d574a44e1486b4dd013
micael <duartemicael32@gmail.com>
2021-05-22 22:57:31 -0300

    test: ensure post is being created

--- backend/tests/models/post_spec.py
+++ backend/tests/models/post_spec.py
@@ -1,7 +1,4 @@
-import time
-from unittest import mock
-from unittest.mock import Mock
-
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.test import TestCase
 from faker import Faker
@@ -15,9 +12,22 @@ class PostTestCase(TestCase):
     def setUp(self) -> None:
         self.faker = Faker()
 
-    def test_to_string_method(self):
+    def test_post_creation(self):
+        """" ensure post is being created """
+        mock_user = mixer.blend(User)
+        post = Post(
+            user=mock_user,
+            title=self.faker.sentence(),
+            body=self.faker.sentence()
+        )
+        post.save()
+        self.assertEqual(Post.objects.count(), 1)
+
+    def test_to_string_representation(self):
         """" ensure __str__ returns a valid to string """
         mock_post = mixer.blend(Post)
         self.assertEqual(str(mock_post), '{}-{}'.format(mock_post.post_id, mock_post.title))
 
-
+    def test_should_have_registered_in_admin(self):
+        """" ensure post model is registered in admin """
+        self.assertTrue(admin.site.is_registered(Post))
