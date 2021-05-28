c0a79cbbef79feb5a7d5f4a91304c1436838e10d
micael <duartemicael32@gmail.com>
2021-05-27 23:24:37 -0300

    feat: login on API via login modal

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,14 +1,21 @@
 (function () {
 
     const ModalForm = {
-        props: ['email', 'password', 'canCancel'],
-        watch:{
-          email(val, oval){
-              console.log(val)
-          }
+        props: {
+            value: {
+                type: Object,
+                required: true
+            },
+            canCancel: String
+        },
+        watch: {
+            value() {
+                console.log(this.value)
+                this.$emit('input', this.value);
+            }
         },
         template: `
-            <form action="">
+            <form action="" style="width: 450px">
                 <div class="modal-card" style="width: auto">
                     <header class="modal-card-head">
                         <p class="modal-card-title">Login</p>
@@ -20,18 +27,19 @@
                     <section class="modal-card-body">
                         <b-field label="Email">
                             <b-input
-                                autocomplete="chrome:off"
+                                autocomplete="new-password"
                                 type="email"
-                                :value="email"
+                                v-model="value.email"
                                 placeholder="Your email">
                             </b-input>
                         </b-field>
                         <b-field label="Password">
                             <b-input
-                                autocomplete="chrome:off"
+                                autocomplete="new-password"
                                 type="password"
-                                :value="password"
+                                v-model="value.password"
                                 password-reveal
+                                @keypress.native.enter="(value.email != '' && value.password != '') ? $emit('submit'): false"
                                 placeholder="Your password">
                             </b-input>
                         </b-field>
@@ -41,7 +49,7 @@
                         <b-button
                             label="Close"
                             @click="$emit('close')" />
-                        <b-button
+                        <b-button :disabled="value.email == '' || value.password == ''"
                             @click="$emit('submit')"
                             label="Login"
                             type="is-primary" />
@@ -61,28 +69,43 @@
                 formProps: {
                     email: '',
                     password: ''
-                }
+                },
+                isLoading: false,
+                isFullPage: false
             }
         },
         methods: {
             submitFormLogin() {
-                const form = new FormData();
-                form.append('client_id', '2EJaS6QcLypGfBtoP2iFtJM6FA90DT0QfaaooIWb');
-                form.append('client_secret', 'oGwGr1fNOnBpmhLh4x72JErlzPzw3JRT4EtJFXAbVkrYqBr4TtVMQ9nbTCQN8jSm2349wa0SDed7TzUfRm2zcUbEdkOe2TjaaABUAEWVOAyTKXXS3gDvEjeCl7jTWo5b');
-                form.append('grant_type', 'password');
-                form.append('username',  this.formProps.email);
-                form.append('password', this.formProps.password);
+                const form = new FormData()
+                form.append('client_id', '2EJaS6QcLypGfBtoP2iFtJM6FA90DT0QfaaooIWb')
+                form.append('client_secret', 'oGwGr1fNOnBpmhLh4x72JErlzPzw3JRT4EtJFXAbVkrYqBr4TtVMQ9nbTCQN8jSm2349wa0SDed7TzUfRm2zcUbEdkOe2TjaaABUAEWVOAyTKXXS3gDvEjeCl7jTWo5b')
+                form.append('grant_type', 'password')
+                form.append('username', this.formProps.email)
+                form.append('password', this.formProps.password)
 
-                axios({
-                    method: 'post',
-                    url: '/oauth/token/',
-                    context: this,
-                    headers: {'content-type': 'multipart/form-data'},
-                    data: form,
-                }).then(({data}) => {
-                    localStorage.setItem('access_token', data.access_token)
-                })
-                this.isComponentModalActive = false
+                this.isLoading = true
+
+                setTimeout(() => {
+                    axios({
+                        method: 'post',
+                        url: '/oauth/token/',
+                        context: this,
+                        headers: {'content-type': 'multipart/form-data'},
+                        data: form,
+                    }).then(({data}) => {
+                        localStorage.setItem('access_token', data.access_token)
+                        this.isComponentModalActive = false
+                    }).catch(({response}) => {
+                        if (response.status) {
+
+                        } else {
+
+                        }
+                    }).then(() => {
+                        this.isLoading = false
+                    })
+                }, 1000)
+
             }
         },
         mounted() {
