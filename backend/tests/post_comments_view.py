9261355ad9c64e9c05d8c8f6e485ce0ab6e798fa
micael <duartemicael32@gmail.com>
2021-05-29 00:13:47 -0300

    test: ensure view returns 400 if no postid is provided post

--- backend/tests/post_comments_view.py
+++ backend/tests/post_comments_view.py
@@ -13,7 +13,6 @@ from rest_framework.test import APIClient, APITransactionTestCase, APITestCase
 from backend.models import Post
 
 
-
 class PostCommentsAPITestCase(APITestCase):
 
     def shortDescription(self):
@@ -48,3 +47,26 @@ class PostCommentsAPITestCase(APITestCase):
         response = self.client.get(url, {}, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_should_return_400_if_no_postid_is_provided_post(self):
+        """ ensure view returns 400 if no postid is provided post """
+        data = {
+            'body': self.faker.paragraph(nb_sentences=3),
+            'user': self.test_user.id
+        }
+        url = reverse('api-post-comments-create', kwargs={'version': 'v1'})
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            "errors": [
+                {
+                    "field": "post",
+                    "message": [
+                        "This field is required."
+                    ]
+                }
+            ]
+        })
+
+
